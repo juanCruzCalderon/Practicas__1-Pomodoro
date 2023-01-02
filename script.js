f79000c8529a98af3fd068db1cba623aec5f0e41
juanCruzCalderon <juancruzcalderoncontacto@gmail.com>
2023-01-02 14:51:51 -0300

    Estructura Basica

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let tempoGiro,gradoGiro=0,frame=document.querySelector(".frame");
+let reloj=document.querySelector(".pomodoro");
+let bola=document.querySelector(".tuki");
+/*
+tempoGiro= setInterval(()=>{
+	gradoGiro++;
+	if (gradoGiro==360) clearInterval(tempoGiro);
+	reloj.style.setProperty('transform', `rotate(${gradoGiro}deg)`);
+} ,Math.floor( 4000/360));
+*/
